a63cbe0d8dfa7124cc6bc7baaee708d9d235406d
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-18 13:51:42 +0000

    Add navstack + form + sections to contentview

--- FrameworkBuildGuide/ContentView.swift
+++ FrameworkBuildGuide/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                //TODO: replace the systemimages with actual images of the laptop, also decorate the view more somehow
+                Section {
+                    NavigationLink {
+                        Text("fw12")
+                    } label: {
+                        Label("Framework 12", systemImage: "laptopcomputer")
+                    }
+                }
+                
+                Section {
+                    NavigationLink {
+                        Text("fw13")
+                    } label: {
+                        Label("Framework 13", systemImage: "laptopcomputer")
+                    }
+                }
+                
+                Section {
+                    NavigationLink {
+                        Text("fw16")
+                    } label: {
+                        Label("Framework 16", systemImage: "laptopcomputer")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
