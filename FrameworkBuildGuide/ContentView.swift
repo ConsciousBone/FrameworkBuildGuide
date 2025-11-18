678cba43932ec5fd24fe4dfd328a72d04a5e27f0
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-18 13:43:44 +0000

    Initial Commit

---
+++ FrameworkBuildGuide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FrameworkBuildGuide
+//
+//  Created by Evan Plant on 18/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FrameworkBuildGuide/FrameworkBuildGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworkBuildGuideApp.swift
+//  FrameworkBuildGuide
+//
+//  Created by Evan Plant on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FrameworkBuildGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
