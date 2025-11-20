43d3aeb6dbe9c8e94a2a749b1bc7cc1690e8bbc5
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-20 17:52:22 +0000

    Add fmm header + make fmm view

---
+++ FrameworkBuildGuide/Views/FMM/FMMMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  FMMMenuView.swift
+//  FrameworkBuildGuide
+//
+//  Created by Evan Plant on 20/11/2025.
+//
+
+import SwiftUI
+
+struct FMMMenuView: View {
+    var body: some View {
+        Text("Find my model view")
+    }
+}
+
+#Preview {
+    FMMMenuView()
+}
