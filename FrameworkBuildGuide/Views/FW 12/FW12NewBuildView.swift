dd15f319068f94387a0ddda8d88d04426b94b715
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-22 01:44:15 +0000

    Make fw12newbuildview

---
+++ FrameworkBuildGuide/Views/FW 12/FW12NewBuildView.swift
@@ -0,0 +1,18 @@
+//
+//  FW12NewBuildView.swift
+//  FrameworkBuildGuide
+//
+//  Created by Evan Plant on 22/11/2025.
+//
+
+import SwiftUI
+
+struct FW12NewBuildView: View {
+    var body: some View {
+        Text("New build view")
+    }
+}
+
+#Preview {
+    FW12NewBuildView()
+}
