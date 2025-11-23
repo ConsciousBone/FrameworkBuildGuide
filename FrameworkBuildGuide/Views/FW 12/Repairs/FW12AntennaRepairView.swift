4e1083ba1bbdc7a18670250eb5ed123245247700
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-23 17:25:20 +0000

    Make fw12antennarepairview

---
+++ FrameworkBuildGuide/Views/FW 12/Repairs/FW12AntennaRepairView.swift
@@ -0,0 +1,18 @@
+//
+//  FW12AntennaRepairView.swift
+//  FrameworkBuildGuide
+//
+//  Created by Evan Plant on 23/11/2025.
+//
+
+import SwiftUI
+
+struct FW12AntennaRepairView: View {
+    var body: some View {
+        Text("Antenna repair view")
+    }
+}
+
+#Preview {
+    FW12AntennaRepairView()
+}
